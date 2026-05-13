52ae31d808a472b6cc1bd2d72725989cf1323bb5
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-05-13 17:58:57 +0300

    first commit

---
+++ MyApp/Person.cs
@@ -0,0 +1,18 @@
+class Person
+{
+    protected string name;
+    protected int age;
+    protected string email;
+    protected string password;
+    protected string phoneNumber;
+
+    Person (string name, int age, string email, string password, string phoneNumber)
+    {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+    
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+int [] numbers = new int [] {1 ,3 ,4 ,5 ,2};
+
+
+Array.Sort(numbers);
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+
+
+ArrayList arrayList = new ArrayList();
+arrayList.Add(1);
+arrayList.Add("Hello");
+arrayList.Add(3.14);
+
+
+foreach (var item in arrayList)
+{
+    Console.WriteLine(item);
+}
+
+
+void printFrom1ToN(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
